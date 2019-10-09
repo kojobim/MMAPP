340818a932058b1007ae04de2027aa9ba0646856
Pedro de Jesus Enriquez <penriquez@mediomelon.mx>
2019-10-09 10:24:02 -0500

    se avanza con proxy de ruteo

---
+++ BIMWso2EICustomMediator/src/main/java/com/bim/custom_mediator/MensajeFormateadorCustom.java
@@ -0,0 +1,20 @@
+package com.bim.custom_mediator;
+
+public class MensajeFormateadorCustom extends AbstractMediator {
+
+    public boolean mediate(MessageContext context) {
+        return true;
+    }
+    
+}
+/***
+{
+    pathVariables: {
+        [name:string]: string
+    },
+    body: "",
+    queryParams: {
+        [name:string]: string
+    }
+}
+ */
