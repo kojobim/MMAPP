7ff33cf8854881c23e98cf669e66ecc1217b72b5
Mónica <msanchez@mediomelon.mx>
2019-10-18 11:04:14 -0500

    Cambios por despliege en desarrollo

--- BIMWso2EIMicroservice/src/main/java/com/bim/msf4j/Application.java
+++ BIMWso2EIMicroservice/src/main/java/com/bim/msf4j/Application.java
@@ -5,9 +5,7 @@ import org.wso2.msf4j.MicroservicesRunner;
 import com.bim.msf4j.ctrl.HSMCtrl;
 import com.bim.msf4j.ctrl.InversionesCtrl;
 import com.bim.msf4j.ctrl.LoginCtrl;
-import com.bim.msf4j.ctrl.PaginadoCtrl;
 import com.bim.msf4j.ctrl.PingCtrl;
-import com.bim.msf4j.ctrl.FiltroInversionesCtrl;
 import com.bim.msf4j.ctrl.TokenCtrl;
 
 public class Application {
@@ -16,7 +14,6 @@ public class Application {
                 .deploy(new PingCtrl(), 
                 		new TokenCtrl(), 
                 		new HSMCtrl(), 
-                		new PaginadoCtrl(),
                 		new LoginCtrl(),
                         new InversionesCtrl())
                 .start();
