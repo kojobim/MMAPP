9547ff86d924cecbcf007c4c0ddbffa4c5371da5
Mónica <msanchez@mediomelon.mx>
2019-11-11 13:57:11 -0600

    Agregando service de avisoPrivacidadVerificar Termino de microservice

--- BIMWso2EICommons/src/main/java/com/bim/commons/service/AvisoPrivacidadServicio.java
+++ BIMWso2EICommons/src/main/java/com/bim/commons/service/AvisoPrivacidadServicio.java
@@ -19,25 +19,90 @@ public class AvisoPrivacidadServicio extends BaseService {
     private static String AvisoPrivacidadServicio;
     
 	private static String AvisoPrivacidadActualizacionOp;
+	private static String AvisoPrivacidadVerificarOp;
+	
 	private static String AvisoPrivacidadActualizacionOpTransaccio;
 	private static String AvisoPrivacidadActualizacionOpUsuario;
 	private static String AvisoPrivacidadActualizacionOpSucOrigen;
 	private static String AvisoPrivacidadActualizacionOpSucDestino;
     private static String AvisoPrivacidadActualizacionOpModulo;
 
+	private static String AvisoPrivacidadVerificarOpTransaccio;
+	private static String AvisoPrivacidadVerificarOpUsuario;
+	private static String AvisoPrivacidadVerificarOpSucOrigen;
+	private static String AvisoPrivacidadVerificarOpSucDestino;
+    private static String AvisoPrivacidadVerificarOpModulo;
+    
 	public AvisoPrivacidadServicio() {
 		super();
 
 		AvisoPrivacidadServicio = properties.getProperty("data_service.aviso_privacidad_servicio");
-        AvisoPrivacidadActualizacionOp = properties.getProperty("aviso_privacidad_servicio.op.aviso_privacidad_actualizacion");
 
+		AvisoPrivacidadActualizacionOp = properties.getProperty("aviso_privacidad_servicio.op.aviso_privacidad_actualizacion");
+		AvisoPrivacidadVerificarOp = properties.getProperty("aviso_privacidad_servicio.op.aviso_privacidad_verificar");
+		
         AvisoPrivacidadActualizacionOpTransaccio = properties.getProperty("op.aviso_privacidad_actualizacion.transaccio");
 		AvisoPrivacidadActualizacionOpUsuario = properties.getProperty("op.aviso_privacidad_actualizacion.usuario");
 		AvisoPrivacidadActualizacionOpSucOrigen = properties.getProperty("op.aviso_privacidad_actualizacion.suc_origen");
 		AvisoPrivacidadActualizacionOpSucDestino = properties.getProperty("op.aviso_privacidad_actualizacion.suc_destino");
         AvisoPrivacidadActualizacionOpModulo = properties.getProperty("op.aviso_privacidad_actualizacion.modulo");
-    }
+    
+        AvisoPrivacidadVerificarOpTransaccio = properties.getProperty("op.aviso_privacidad_verificar.transaccio");
+        AvisoPrivacidadVerificarOpUsuario = properties.getProperty("op.aviso_privacidad_verificar.usuario");
+        AvisoPrivacidadVerificarOpSucOrigen = properties.getProperty("op.aviso_privacidad_verificar.suc_origen");
+        AvisoPrivacidadVerificarOpSucDestino = properties.getProperty("op.aviso_privacidad_verificarsuc_destino");
+        AvisoPrivacidadVerificarOpModulo = properties.getProperty("op.aviso_privacidad_verificar.modulo");
+	}
 
+    /**
+     * Método para verificar el aviso de privacidad
+     * ProcedureName: NBENRMOVCON
+     * @param datosAvisoPrivacidad
+     * <pre>
+     * {
+     *	Usu_Numero: String,
+     *	NumTransac: String,
+     *	Transaccio: String,
+     *	Usuario: String,
+     *	FechaSis: String,
+     *	SucOrigen: String,
+     *	SucDestino: String,
+     *	Modulo: String
+     * }
+     * </pre>
+     * @return
+     * <pre>
+     * {
+     *	avisoPrivacidad: {
+     *		Usu_Numero: String, 
+     *		Usu_Client: String,
+     *		Usu_AceAvi: Boolean, 
+     *		Usu_FecAce: Date,
+     *		Usu_FecAct: Date, 
+     *		Usu_Enrola: Boolean,
+     *		NumTransac: String, 
+     *		Transaccio: String,
+     *		Usuario: String, 
+     *		FechaSis: Date,
+     *		SucOrigen: String, 
+     *		SucDestino: String,
+     *		Modulo: String
+     *	}
+     * }
+     * </pre>
+     */
+	public JsonObject avisoPrivacidadVerificar(JsonObject datosAvisoPrivacidad) {
+		logger.info("COMMONS: Comenzando avisoPrivacidadVerificar metodo...");
+		datosAvisoPrivacidad.addProperty("Transaccio", AvisoPrivacidadVerificarOpTransaccio);
+        datosAvisoPrivacidad.addProperty("Usuario", AvisoPrivacidadVerificarOpUsuario);
+        datosAvisoPrivacidad.addProperty("SucOrigen", AvisoPrivacidadVerificarOpSucOrigen);
+        datosAvisoPrivacidad.addProperty("SucDestino", AvisoPrivacidadVerificarOpSucDestino);
+        datosAvisoPrivacidad.addProperty("Modulo", AvisoPrivacidadVerificarOpModulo);
+		JsonObject result = Utilerias.performOperacion(AvisoPrivacidadServicio, AvisoPrivacidadVerificarOp, datosAvisoPrivacidad);
+		logger.info("COMMONS: Finalizando avisoPrivacidadVerificar metodo...");
+		return result;
+    }//Cierre del método
+	
 	
     /**
      * Método para actualizar el de aviso de privacidad
--- BIMWso2EIMicroservice/src/main/java/com/bim/msf4j/ctrl/AvisoPrivacidadCtrl.java
+++ BIMWso2EIMicroservice/src/main/java/com/bim/msf4j/ctrl/AvisoPrivacidadCtrl.java
@@ -11,7 +11,11 @@ import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
 import org.wso2.msf4j.Request;
+
 import com.bim.commons.service.AvisoPrivacidadServicio;
+import com.bim.commons.service.TransaccionServicio;
+import com.bim.commons.utils.Utilerias;
+import com.google.gson.JsonObject;
 
 @Path("/")
 public class AvisoPrivacidadCtrl extends BimBaseCtrl {
@@ -19,11 +23,13 @@ public class AvisoPrivacidadCtrl extends BimBaseCtrl {
 	private static final Logger logger = Logger.getLogger(AvisoPrivacidadCtrl.class);
 
 	private AvisoPrivacidadServicio avisoPrivacidadServicio;
+	private TransaccionServicio transaccionServicio;
 	
 	public AvisoPrivacidadCtrl() {
 		super();
 		logger.info("CTRL: Comenzando metodo init...");
-		this.avisoPrivacidadServicio = new AvisoPrivacidadServicio();		
+		this.avisoPrivacidadServicio = new AvisoPrivacidadServicio();
+		this.transaccionServicio = new TransaccionServicio();
 		logger.info("CTRL: Terminando metodo init...");
 	}
 	
@@ -39,4 +45,50 @@ public class AvisoPrivacidadCtrl extends BimBaseCtrl {
 				.build();
 	}
 	
+	@Path("usuario/aviso-privacidad")
+	@GET()
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response verificarAvisoPrivacidad(@Context Request solicitud) {
+		logger.info("CTRL: Comenzando verificarAvisoPrivacidad metodo...");
+		
+		logger.info("Authorization " + solicitud.getHeader("Authorization"));
+
+		String bearerToken = solicitud.getHeader("Authorization");
+		JsonObject principalResultadoObjeto = Utilerias.obtenerPrincipal(bearerToken);
+		logger.info("- principal " + principalResultadoObjeto);
+
+		JsonObject folioTransaccionGenerarResultadoObjeto = this.transaccionServicio.folioTransaccionGenerar();
+		
+		JsonObject transaccion = Utilerias.obtenerJsonObjectPropiedad(folioTransaccionGenerarResultadoObjeto,"transaccion");
+		String folTransa = Utilerias.obtenerStringPropiedad(transaccion,"Fol_Transa");
+		logger.info("- folTransa " + folTransa);
+
+		String fechaSis = Utilerias.obtenerFechaSis();
+		
+		String usuNumero = principalResultadoObjeto.get("usuNumero").getAsString();
+		
+		JsonObject datosAvisoPrivacidad = new JsonObject();
+		datosAvisoPrivacidad.addProperty("Usu_Numero", usuNumero);
+		datosAvisoPrivacidad.addProperty("NumTransac", folTransa);
+		datosAvisoPrivacidad.addProperty("FechaSis", fechaSis);
+		
+		JsonObject avisoPrivacidadVerificarObjeto = this.avisoPrivacidadServicio.avisoPrivacidadVerificar(datosAvisoPrivacidad);
+		logger.info("- avisoPrivacidadVerificarObjeto " + avisoPrivacidadVerificarObjeto);
+		
+		JsonObject avisoPrivacidad = Utilerias.obtenerJsonObjectPropiedad(avisoPrivacidadVerificarObjeto, "avisoPrivacidad");
+		
+		Boolean usuAceAvi = Boolean.parseBoolean(Utilerias.obtenerStringPropiedad(avisoPrivacidad,"Usu_AceAvi"));
+		logger.info("- usuAceAvi " + usuAceAvi);
+
+		JsonObject avisoPrivacidadResultadoObjeto = new JsonObject();
+		avisoPrivacidadResultadoObjeto.addProperty("cpAvisoAceptado", usuAceAvi);
+		
+		JsonObject avisoPrivacidadAceptado = new JsonObject();
+		avisoPrivacidadAceptado.add("avisoPrivacidadAceptado", avisoPrivacidadResultadoObjeto);
+		logger.info("CTRL: Finalizando verificarAvisoPrivacidad metodo");
+		return Response
+				.ok(avisoPrivacidadAceptado.toString(), MediaType.APPLICATION_JSON)
+				.build();	
+	}
 }
