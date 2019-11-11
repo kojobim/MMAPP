1d20392b5d44f73457f3d9cfa9627fdad4266384
edwinbalseca <ebalseca@mediomelon.mx>
2019-11-11 14:01:12 -0600

    Aceptar aviso de privacidad terminado

--- BIMWso2EIMicroservice/src/main/java/com/bim/msf4j/ctrl/AvisoPrivacidadCtrl.java
+++ BIMWso2EIMicroservice/src/main/java/com/bim/msf4j/ctrl/AvisoPrivacidadCtrl.java
@@ -1,6 +1,7 @@
 package com.bim.msf4j.ctrl;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -14,7 +15,9 @@ import org.wso2.msf4j.Request;
 import com.bim.commons.dto.BimMessageDTO;
 import com.bim.commons.enums.AvisoPrivacidadFormatosEnum;
 import com.bim.commons.exceptions.BadRequestException;
+import com.bim.commons.exceptions.InternalServerException;
 import com.bim.commons.service.AvisoPrivacidadServicio;
+import com.bim.commons.service.TransaccionServicio;
 import com.bim.commons.utils.Utilerias;
 import com.google.gson.JsonObject;
 
@@ -24,11 +27,15 @@ public class AvisoPrivacidadCtrl extends BimBaseCtrl {
 	private static final Logger logger = Logger.getLogger(AvisoPrivacidadCtrl.class);
 
 	private AvisoPrivacidadServicio avisoPrivacidadServicio;
+	private TransaccionServicio transaccionServicio;
 	
 	public AvisoPrivacidadCtrl() {
 		super();
 		logger.info("CTRL: Comenzando metodo init...");
-		this.avisoPrivacidadServicio = new AvisoPrivacidadServicio();		
+		
+		this.avisoPrivacidadServicio = new AvisoPrivacidadServicio();
+		this.transaccionServicio = new TransaccionServicio();
+		
 		logger.info("CTRL: Terminando metodo init...");
 	}
 	
@@ -62,6 +69,48 @@ public class AvisoPrivacidadCtrl extends BimBaseCtrl {
 		
 		logger.info("CTRL: Terminando obtenerAvisoPrivacidad metodo");
 		return Response.ok(resultado.toString(), MediaType.APPLICATION_JSON)
+				.build();
+	}
+	
+	@Path("/usuario/aviso-privacidad")
+	@POST()
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response aceptarAvisoPrivacidad(@Context final Request solicitud) {
+		logger.info("CTRL: Comenzando aceptarAvisoPrivacidad metodo...");
+		
+		String bearerToken = solicitud.getHeader("Authorization");
+		JsonObject principalResultadoObjecto = Utilerias.obtenerPrincipal(bearerToken);
+		
+		String usuNumero = principalResultadoObjecto.get("usuNumero").getAsString();
+		String usuClient = principalResultadoObjecto.get("usuClient").getAsString();
+		
+		JsonObject folioTransaccionGenerarResultadoObjeto = this.transaccionServicio.folioTransaccionGenerar();
+		logger.info("folioTransaccionGenerarResultadoObjeto" + folioTransaccionGenerarResultadoObjeto);
+
+		String FolioTransaccionGenerarFolTransa = folioTransaccionGenerarResultadoObjeto.get("transaccion").getAsJsonObject().get("Fol_Transa").getAsString();
+		String fechaSis = Utilerias.obtenerFechaSis();
+		
+		JsonObject datosAvisoPrivacidad = new JsonObject();
+		datosAvisoPrivacidad.addProperty("Usu_Numero", usuNumero);
+		datosAvisoPrivacidad.addProperty("Usu_Client", usuClient);
+		datosAvisoPrivacidad.addProperty("Usu_FecAce", fechaSis);
+		datosAvisoPrivacidad.addProperty("Usu_FecAct", fechaSis);
+		datosAvisoPrivacidad.addProperty("NumTransac", FolioTransaccionGenerarFolTransa);
+		datosAvisoPrivacidad.addProperty("FechaSis", fechaSis);
+		
+		JsonObject avisoPrivacidadActualizacionResultado = avisoPrivacidadServicio.avisoPrivacidadActualizacion(datosAvisoPrivacidad);
+		JsonObject avisoPrivacidadActualizacionResultadoObjeto = Utilerias.obtenerJsonObjectPropiedad(avisoPrivacidadActualizacionResultado, "avisoPrivacidad");
+		String errCodigo = Utilerias.obtenerStringPropiedad(avisoPrivacidadActualizacionResultadoObjeto, "Err_Codigo");
+		
+		if(!"000000".equals(errCodigo)) {
+			String errMensaj = Utilerias.obtenerStringPropiedad(avisoPrivacidadActualizacionResultadoObjeto, "Err_Mensaj");			
+			BimMessageDTO bimMessageDTO = new BimMessageDTO("BIM.MENSAJ.31");
+			bimMessageDTO.addMergeVariable("errMensaj", errMensaj);
+			throw new InternalServerException(bimMessageDTO.toString());
+		}
+		
+		logger.info("CTRL: Terminando aceptarAvisoPrivacidad metodo...");
+		return Response.noContent()
 				.build();
 	}
 	
