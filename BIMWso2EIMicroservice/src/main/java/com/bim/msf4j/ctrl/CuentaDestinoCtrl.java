4003062409a8ceef923202742305948b57da7952
Mónica <msanchez@mediomelon.mx>
2019-11-11 18:05:04 -0600

    Microservices verificar cuenta destino

--- BIMWso2EIMicroservice/src/main/java/com/bim/msf4j/ctrl/CuentaDestinoCtrl.java
+++ BIMWso2EIMicroservice/src/main/java/com/bim/msf4j/ctrl/CuentaDestinoCtrl.java
@@ -7,13 +7,19 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
+import org.wso2.msf4j.Request;
 
+import com.bim.commons.dto.BimMessageDTO;
+import com.bim.commons.exceptions.BadRequestException;
+import com.bim.commons.exceptions.ConflictException;
 import com.bim.commons.service.CuentaDestinoServicio;
+import com.bim.commons.utils.Utilerias;
+import com.google.gson.JsonObject;
 
+@Path("/cuentas-destino")
 public class CuentaDestinoCtrl extends BimBaseCtrl {
 
 	private static final Logger logger = Logger.getLogger(CuentaDestinoCtrl.class);
@@ -27,15 +33,55 @@ public class CuentaDestinoCtrl extends BimBaseCtrl {
 		logger.info("CTRL: Finalizando metodo init...");		
 	}
 	
-	@Path("/cuentas-destino/beneficiarios")
+	@Path("/beneficiarios")
 	@GET
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response cuentaDestinoVerificar(@QueryParam("cpCuenta") String cuenta, @Context final Request solicitud) {
+	public Response cuentaDestinoVerificar(@QueryParam("cpCuenta") String cpCuenta, @Context final Request solicitud) {
 		logger.info("CTRL: Comenzando cuentaDestinoVerificar metodo...");
 		
-		logger.info("CTRL: Finalizando cuentaDestinoVerificar metodo...");
-		return Response.ok(MediaType.APPLICATION_JSON)
+		String bearerToken = solicitud.getHeader("Authorization");
+		JsonObject principalResultadoObjecto = Utilerias.obtenerPrincipal(bearerToken);
+		
+		if(cpCuenta == null || cpCuenta.isEmpty()) {
+			BimMessageDTO bimMessageDTO = new BimMessageDTO("BIM.MENSAJ.45");
+			throw new BadRequestException(bimMessageDTO.toString());
+		}
+		
+		String fechaSis = Utilerias.obtenerFechaSis();
+		
+		JsonObject datosCuentaDestinoVerificar = new JsonObject();
+		datosCuentaDestinoVerificar.addProperty("Ces_Cuenta", cpCuenta);
+		datosCuentaDestinoVerificar.addProperty("FechaSis", fechaSis);
+		
+		JsonObject cuentaDestinoVerificarResultadoObjeto = this.cuentaDestinoServicio.cuentasEspecialesConsultar(datosCuentaDestinoVerificar);
+		logger.info("- cuentaDestinoVerificarResultadoObjeto: " + cuentaDestinoVerificarResultadoObjeto);
+		
+		JsonObject cuentaDestinoObjeto = Utilerias.obtenerJsonObjectPropiedad(cuentaDestinoVerificarResultadoObjeto, "cuentasEspeciales");
+		logger.info("- cuentaDestinoObjeto: " + cuentaDestinoObjeto);
+
+		JsonObject cuentaDestinoVerificarObjeto = new JsonObject();
+		if(cuentaDestinoObjeto.has("Cue_Status")) {
+			String cueStatus = Utilerias.obtenerStringPropiedad(cuentaDestinoObjeto, "Cue_Status");
+			if(!cueStatus.equals("A")) {
+				BimMessageDTO bimMessageDTO = new BimMessageDTO("BIM.MENSAJ.47");
+				bimMessageDTO.addMergeVariable("cuenta", cpCuenta);
+				throw new ConflictException(bimMessageDTO.toString());
+			}
+
+			String cueNumero = Utilerias.obtenerStringPropiedad(cuentaDestinoObjeto, "Cue_Numero");
+			String cliComOrd = Utilerias.obtenerStringPropiedad(cuentaDestinoObjeto, "Cli_ComOrd");
+
+			cuentaDestinoVerificarObjeto.addProperty("cueNumero", cueNumero);
+			cuentaDestinoVerificarObjeto.addProperty("cliComOrd", cliComOrd);
+			logger.info("- cuentaDestinoVerificarObjeto " + cuentaDestinoVerificarObjeto);
+		}
+
+		JsonObject cuentaDestinoVerificarResultado = new JsonObject();
+		cuentaDestinoVerificarResultado.add("beneficiario", cuentaDestinoVerificarObjeto);
+		logger.info("CTRL: Terminando cuentaDestinoVerificar metodo");
+		return Response
+				.ok(cuentaDestinoVerificarResultado.toString(), MediaType.APPLICATION_JSON)
 				.build();
 	}
 	
